d04e4866a518a276f62d9c230b8a4f6468849685
Reigits <rafarobyan1@gmail.com>
2026-09-22 12:43:53 +0700

    feat: menambahkan urls di urls.py untuk method star.

--- main/urls.py
+++ main/urls.py
@@ -20,4 +20,9 @@ urlpatterns = [
     path("register/", register, name="register"),
     path("login/", login_user, name="login"),
     path("logout/", logout_user, name="logout"),
+    path(
+    "projects/<uuid:project_id>/star/",
+    toggle_star,
+    name="toggle_star",
+),
 ]
