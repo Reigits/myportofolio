53352256d29f608fbf318fcb80410005acd06c20
Reigits <rafarobyan1@gmail.com>
2026-09-22 12:36:47 +0700

    feat: menambahkan system star pada model project.

--- main/models.py
+++ main/models.py
@@ -67,6 +67,10 @@ class Project(models.Model):
     tech_stack = models.CharField(max_length=255)
     project_url = models.URLField(blank=True)
     project_image_url = models.URLField(blank=True, max_length=500)
+    # system star
+    starred_by = models.ManyToManyField(
+        User, related_name="starred_projects", blank=True
+    )
 
     def __str__(self):
         return self.title
