f0ba84dae53b3586169c1dd58d10745cbe618d31
Reigits <rafarobyan1@gmail.com>
2026-09-23 08:49:06 +0700

    feat: modifikasi method get_project_json agar menampilkan nama user yang men star project.

--- main/views.py
+++ main/views.py
@@ -78,7 +78,7 @@ def get_projects_json(request):
     if title_query:
         projects = projects.filter(title__icontains=title_query)
 
-    projects_json = serializers.serialize("json", projects)
+    projects_json = serializers.serialize("json", projects, use_natural_foreign_keys = True)
     return HttpResponse(projects_json, content_type="application/json")
 
 # karena form menjadi popover dan bukan halaman terpisah, ini dipake biar gak perlu ada project/add di url
